22440a03006a954a7ce6c57977486b4b6f1f4820
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-05-29 16:43:36 +0800

    [JS-2] My Solution to Activity Number two for javascript.

--- week-1/javascript/2.js
+++ week-1/javascript/2.js
@@ -3,3 +3,12 @@
  * @param {number[]} arr The array of numbers.
  * @return {number} The sum of all numbers in the array.
  */
+const sumFn = (arr) => {
+    let initail_number = 0;
+    for(let number of arr){
+        
+        initail_number+=number;
+    }
+    return initail_number;
+}
+console.log(sumFn([1,2,3]));
