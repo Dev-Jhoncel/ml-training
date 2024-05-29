71267d279fa4376bcb76c432e71438d408b50c1d
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-05-29 16:38:39 +0800

    [JS-1] My Solution to Activity Number one for javascript.

--- week-1/javascript/1.js
+++ week-1/javascript/1.js
@@ -3,3 +3,7 @@
  * @param {number} num The number to check.
  * @return {boolean} True if the number is even, false if it is odd.
  */
+const checkNum = (num) => {
+    return (num % 2 === 0)? true : false;
+ }
+ console.log(checkNum(2));
