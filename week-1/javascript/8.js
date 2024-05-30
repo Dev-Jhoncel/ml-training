3d942594ef09e545bd17f78f21b1878277df0484
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-05-30 09:45:41 +0800

    [JS-8] A function that will return all vowels from the given string

--- week-1/javascript/8.js
+++ week-1/javascript/8.js
@@ -1,5 +1,9 @@
 /**
  * Write a function that returns all vowels in a string.
  * @param {string} str The string to check.
- * @return {string} The string with all vowels removed.
+ * @return {string} The string with all vowels.
  */
+const getVowels = (str) => {
+        let vowels = /[aeiou]/;
+        return [...str].map(char => char.match(vowels)).join('');
+}
