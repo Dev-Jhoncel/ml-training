1421b1a3bcb43896d995db612ffa7a94b9b6fd5b
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-05-30 10:17:03 +0800

    [JS-10] A function that will thrown an error if the second parameter is equal to 0

--- week-1/javascript/10.js
+++ week-1/javascript/10.js
@@ -4,3 +4,20 @@
  * @param {number} num2 The second number to check.
  * @throws {Error} If the second number is 0.
  */
+const checkZero = (num1,num2) => {
+    try
+    {
+        if(typeof num1 === "number" && typeof num2 === "number")
+        {
+            if(num2 === 0) throw new Error('The second number is a 0')
+            return `Both inputs are valid`
+        }
+        else
+        {
+            throw new Error('The inputted parameter is not a number')
+        }
+    }
+    catch(error){
+        return error.message
+    }
+}
