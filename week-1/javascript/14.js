694efcdfd79c6a53909b1d0ec6eed3752a072ef9
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-05-31 09:44:55 +0800

    [JS-14]A function that accepts an array of numbers and displays the highest and lowest numbers in the array

--- week-1/javascript/14.js
+++ week-1/javascript/14.js
@@ -6,3 +6,18 @@
  * Example array: [5, 2, 8, 1, 9, 3]
  * Expected output: "The lowest number is 1 and the highest number is 9."
  */
+const arraySort = (arr) => {
+    try
+    {
+        if(!(arr instanceof Array)) throw new Error('Inputs is not an Array')
+        arr.map(num => { if(typeof num !== "number") throw new Error('Encountered a value with a non number data types') });
+        arr = arr.sort((a,b) => a - b);
+        return `The lowest number is ${arr[0]} and the highest number is ${arr[arr.length - 1]}.`
+    }
+    catch(error)
+    {
+        return error.message;
+    }
+}
+
+console.log(arraySort([2,5,8,1,1.1,0.5,-1]));
