70a6a1961f8a1218026e2d6075dd04be22ea0e35
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-06-01 16:39:41 +0800

    [TS-12]A function that takes an array of numbers and throws a custom 'Error' if the array is empty.

--- week-1/typescript/12.ts
+++ week-1/typescript/12.ts
@@ -3,3 +3,22 @@
  * @param {number[]} arr The array to check.
  * @throws {Error} If the array is empty.
  */
+
+const checkEmptyArray = (arr:number[]):string => {
+    try
+    {
+        if(arr instanceof Array)
+        {
+            if(arr.length === 0) throw new Error('Array is empty');
+            arr.map(num => { if(typeof num !== "number") throw new Error('Encountered a value with a non number data types') });
+            return 'Array is not empty'
+        }
+        return 'Parameter is not an Array'
+    }
+    catch(error)
+    {
+        return (error as Error).message;
+    }
+}
+
+console.log(checkEmptyArray([]));
