1d9f7834cb31827b73277d7c74ae9a7b5537e92d
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-06-01 16:06:43 +0800

    [TS-4]a function to display the current date and time in the format

--- week-1/typescript/4.ts
+++ week-1/typescript/4.ts
@@ -3,3 +3,12 @@
  * 'MM-DD-YYYY HH:MM:SS'.
  * @return {string} The current date and time in the format 'MM-DD-YYYY HH:MM:SS'.
  */
+const currentDate  = (): string => {
+    let date = new Date();
+    let current_month = (String(date.getMonth()).length === 2)? date.getMonth() : '0' + date.getMonth();
+    let current_day = (String(date.getDate()).length === 2)? date.getDate() : '0' + date.getMonth();
+    let current_date = `${current_month}-${current_day}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    return `${current_date}`;
+} 
+
+console.log(currentDate());
