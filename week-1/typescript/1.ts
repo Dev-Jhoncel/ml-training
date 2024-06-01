12bafceaff1bc4d778b443c41a3cd50ef66dd049
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-06-01 15:51:37 +0800

    [TS-1] a function to check if a number is even or odd

--- week-1/typescript/1.ts
+++ week-1/typescript/1.ts
@@ -3,3 +3,8 @@
  * @param {number} num The number to check.
  * @return {boolean} True if the number is even, false if it is odd.
  */
+const checkNumFunc = (num: number): boolean  => {
+    return (num % 2 === 0)? true : false;
+};
+
+console.log(checkNumFunc(1));
