096e872a9a36435012d431fc37880554d9053e5b
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-06-01 16:47:13 +0800

    [TS-15]A function for loop that iterates from 1 to a given number. It then checks and outputs whether the number is even or odd.

--- week-1/typescript/15.ts
+++ week-1/typescript/15.ts
@@ -2,3 +2,11 @@
  * Write a function for loop that iterates from 1 to a given number. It then checks and outputs whether the number is even or odd.
  * @param {number} n The number to check.
  */
+const countNumber = (n:number):void => {
+    for(let i = 1; i <= n; i++)
+    {
+        console.log(`The number ${i} is ${(i % 2 === 0)? "even" : "odd"}`);
+    }
+}
+
+countNumber(10);
