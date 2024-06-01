700ca4c13f576332ae78d86be525617024f6c954
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-06-01 15:38:23 +0800

    [JS-20]A function that accepts a string as input and swaps the case of each character in the string

--- week-1/javascript/20.js
+++ week-1/javascript/20.js
@@ -7,3 +7,14 @@
  * @param {string} str
  * @returns {string}
  */
+const fromatString = (str) => {
+
+    let newString = [...str]
+    let format_string = "";
+    for(let newstr of newString)
+    {
+        format_string += `${(/[A-Z]/g).test(newstr)? newstr.toLowerCase() : newstr.toUpperCase() }`;
+    }
+    return format_string
+}
+console.log(fromatString("Hello World"));
