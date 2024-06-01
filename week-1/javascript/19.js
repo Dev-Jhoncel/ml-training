d0ca2f7756cedf55e0e7af1c2b583b81522701ff
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-06-01 15:08:52 +0800

    [JS-19]A function that merges two arrays into a single array and removes duplicates

--- week-1/javascript/19.js
+++ week-1/javascript/19.js
@@ -9,3 +9,17 @@
  * @param {number[]} arr2
  * @returns {number[]}
  */
+
+const mergeArrays = (arr1,arr2) => {
+    let merge_arrays = [...arr1,...arr2]
+    let removeDuplicate = [];
+    for(let num of merge_arrays)
+    {
+      if(!(removeDuplicate.includes(num)))
+      {
+        removeDuplicate.push(num)
+      }
+    }
+return removeDuplicate;
+} 
+console.log(mergeArrays([1, 2, 3],[2, 3, 4]));
