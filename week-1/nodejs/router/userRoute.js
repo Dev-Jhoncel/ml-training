f5d288931fdc2bd15c233ca23aa5fc96d2688934
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2024-06-02 12:57:32 +0800

    [NodeJS]A basic REST API that creates, updates, deletes, and retrieves a user.

--- week-1/nodejs/router/userRoute.js
+++ week-1/nodejs/router/userRoute.js
@@ -10,6 +10,30 @@ router.get('/:id', getSpecificUser);
 router.post('/', insertNewUser);
 
 router.put('/:id', updateSpecificUser);
+//Params and Body for updateSpecificUser endpoint
+//Request Params: id
+//Request Body:
+// {
+//     "name": "Jhoncel Cadiena",
+//     "email": "jhoncel.cadiena@example.com",
+//     "age": 25,
+//     "address": {
+//       "street": "123 Main St",
+//       "city": "Anytown",
+//       "state": "CA",
+//       "zip": "12345"
+//     },
+//     "phoneNumbers": [
+//       {
+//         "type": "home",
+//         "number": "09959280777"
+//       },
+//       {
+//         "type": "work",
+//         "number": "555-5678"
+//       }
+//     ]
+//   }
 
 router.delete('/:id', deleteSpecificUser);
 
