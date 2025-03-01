d5a33f2e355d51496543b954e5a12938ac17c193
Dev-Jhoncel <kadiina94@gmail.com>
2025-03-01 19:38:34 +0800

    Add todo component to main page.

--- todo-list/src/app/Main/index.tsx
+++ todo-list/src/app/Main/index.tsx
@@ -1,10 +1,11 @@
 import { Footer } from "../Footer";
+import { Todo } from "../../components/todo"
 
 export const Main = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start sm:flex-row">
-        Body
+        <Todo/>
       </main>
       <Footer />
     </div>
