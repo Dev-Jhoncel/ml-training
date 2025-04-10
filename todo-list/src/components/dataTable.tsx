939e11a1d6c57f1771f6b91dfe83bd84616e3eda
Jhoncel S. Cadiena <89240560+Dev-Jhoncel@users.noreply.github.com>
2025-04-10 17:53:56 +0800

    [Modify][Jhoncel]Modify code .

--- todo-list/src/components/dataTable.tsx
+++ todo-list/src/components/dataTable.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { DataTableProps } from "../lib/interfaces/datatables.interface";
 
+//Dynamic DataTable component
+// This component is a generic data table that takes in columns and data as props.
 export const DataTable = <T extends { [key: string]: any }>({
   columns,
   data,
@@ -8,34 +10,34 @@ export const DataTable = <T extends { [key: string]: any }>({
 }: DataTableProps<T>) => {
   return (
     <div className="overflow-x-auto">
-    <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
-      <thead className="bg-gray-200">
-        <tr>
-          {columns.map((column: any) => (
-            <th
-              key={String(column)}
-              className="py-3 px-4 text-left text-gray-600 font-semibold"
-            >
-              {String(column).toUpperCase()}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item: any) => (
-          <tr className="hover:bg-gray-100" key={String(item[keyField])}>
+      <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
+        <thead className="bg-gray-200">
+          <tr>
             {columns.map((column: any) => (
-              <td
+              <th
                 key={String(column)}
-                className="py-3 px-4 border-b border-gray-200"
+                className="py-3 px-4 text-left text-gray-600 font-semibold"
               >
-                {item[column]}
-              </td>
+                {String(column).toUpperCase()}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
+        </thead>
+        <tbody>
+          {data.map((item: any) => (
+            <tr className="hover:bg-gray-100" key={String(item[keyField])}>
+              {columns.map((column: any) => (
+                <td
+                  key={String(column)}
+                  className="py-3 px-4 border-b border-gray-200"
+                >
+                  {item[column]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
