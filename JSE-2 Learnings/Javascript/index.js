b907148642ab537ce441c7406202eda8a32d5dce
Jhoncel Cadiena <jhoncel.cadiena@mlhuillier.com>
2025-06-19 15:06:30 +0800

    Class explaination.

--- JSE-2 Learnings/Javascript/index.js
+++ JSE-2 Learnings/Javascript/index.js
@@ -138,4 +138,23 @@ images.show();
 images.clear();
 images.show();
 
-                 
+
+let Vehicle = function(initialData){
+let {id, latitude, longitude} = initialData;
+
+this.setPosition = function(latitude, longitude) {
+    this.time = Date.now();
+    this.longitude = longitude;
+    this.latitude = latitude;
+};
+
+this.id = id;
+this.status = "unavailable"; 
+this.setPosition(latitude, longitude);
+};
+
+let vehicle1 = new Vehicle({id: "AL1024", latitude: 59.367647, longitude: 18.213451});
+let vehicle2 = new Vehicle({longitude: 18.213423, latitude: 59.367628, id: "AL1024"});
+
+console.log(vehicle1.id); // AL1024
+console.log(vehicle2); // Vehicle { id: 'AL1024', status: 'unavailable', setPosition: [Function] }
