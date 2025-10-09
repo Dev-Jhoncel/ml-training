24704ed8b84071241e174562496e938ec0679846
Jhoncel Cadiena <jhoncel.cadiena@mlhuillier.com>
2025-10-09 21:13:38 +0800

    Day 3 Practice

--- week-3/todo-list/src/app/page.tsx
+++ week-3/todo-list/src/app/page.tsx
@@ -1,23 +1,38 @@
 export default function Home() {
   return (
     <>
-      <div className="flex gap-[2%] flex-wrap content-start">
-        <nav className="w-full h-[5%] border-b-stone-300 bg-white p-2 text-center font-bold">
+      <div className="flex gap-[2%] flex-wrap content-start min-h-screen">
+        {/* Navbar */}
+        <nav className="w-full h-16 border-b-stone-300 bg-white p-2 text-center font-bold shadow-sm">
           <h1 className="text-3xl font-bold">
-            <span>📝</span>
-            todo-list
+            <span>📝</span> todo-list
           </h1>
         </nav>
-        <aside className="w-1/4 h-3/4"></aside>
-        <main className="grow h-3/4 ">
+
+        {/* Sidebar */}
+        <aside className="fixed lg:flex flex-col w-16 h-full bg-white border-r-stone-300 p-2 text-center gap-4 hidden lg:block">
+          <i>1</i>
+          <i>2</i>
+          <i>3</i>
+          <i>4</i>
+          <i>5</i>
+        </aside>
+
+        {/* Main Content */}
+        <main className="grow h-full flex flex-col justify-between p-4 md:p-8">
           <input
-            className="border h-10 border-stone-300 rounded-md p-2 w-full md:w-1/2"
+            className="border h-10 border-stone-300 rounded-md p-2 w-full md:w-1/2 lg:w-1/3"
             type="text"
             name="txtBox"
             id="txBoxId"
+            placeholder="Add a new task..."
           />
         </main>
-        <div className="w-full h-[5%] bg-white">Footer</div>
+
+        {/* Footer */}
+        <div className="w-full h-16 border-t-stone-300 bg-white text-center flex items-center justify-center">
+          Footer
+        </div>
       </div>
     </>
   );
